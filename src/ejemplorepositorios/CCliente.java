fbd40397bf2760d5bf18815abe514a88c72d439f
dam06 <dam06@PC06.aula1.com>
2019-04-25 11:14:50 +0200

    Esta es la segunda version

---
+++ src/ejemplorepositorios/CCliente.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplorepositorios;
+
+/**
+ *
+ * @author dam06
+ */
+public class CCliente {
+    private String nombre,apellido1,apellido2;
+
+    public CCliente() {
+    }
+
+    public CCliente(String nombre, String apellido1, String apellido2) {
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+    }
+    
+}
